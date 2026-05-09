65b6691d39474b49d5cc5ef40aac6733cf44a0c5
ResulShafili <qiyasogluzefer@gmail.com>
2026-05-09 22:37:26 +0400

    Fix pycodestyle

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python3
-""" Provides some stats about Nginx logs stored in MongoDB """
+"""Script that provides some stats about Nginx logs stored in MongoDB."""
 from pymongo import MongoClient
 
 
-def log_stats():
-    """ Provides some stats about Nginx logs stored in MongoDB """
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    # Ümumi loqların sayı
-    total_logs = collection.count_documents({})
-    print("{} logs".format(total_logs))
+    total = collection.count_documents({})
+    print(f"{total} logs")
 
-    # Metodlar üzrə statistika
     print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        print(f"\tmethod {method}: {count}")
 
-    # Status check (method=GET, path=/status) sayı
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status_check))
-
-
-if __name__ == "__main__":
-    log_stats()
+    status = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status} status check")
