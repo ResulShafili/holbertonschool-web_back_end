43a9cfa732cf9d63f0d04e0b3646424bc4fc6995
ResulShafili <qiyasogluzefer@gmail.com>
2026-05-16 22:30:36 +0400

    Create 6-set.js

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a Set from an array.
+ * @param {Array} array - An array of any kind of element.
+ * @returns {Set} A Set containing the unique elements of the array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
