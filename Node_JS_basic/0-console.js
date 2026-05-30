1ae2a04a163a769b9081f6236cff495e3d8a8103
ResulShafili <qiyasogluzefer@gmail.com>
2026-05-30 22:56:29 +0400

    Create 0-console.js

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints a message to the standard output (STDOUT).
+ * @param {string} message - The message to display.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
