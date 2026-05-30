5d743fc49811c7a62fb2196beba6664c09369fc0
ResulShafili <qiyasogluzefer@gmail.com>
2026-05-30 23:41:02 +0400

    Update server.js

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,16 +1,13 @@
-// create a small Express server:
-
-// It should use the routes defined in full_server/routes/index.js
-// to serve the should use the port 1245
-
 import express from 'express';
-
-const routes = require('./routes/index');
+import router from './routes';
 
 const app = express();
+const port = 1245;
 
-app.use(routes);
+app.use('/', router);
 
-app.listen(1245);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
 export default app;
