957eabd55bf7a1ef3e8fe61184b6b7125d1378dd
ResulShafili <qiyasogluzefer@gmail.com>
2026-05-30 23:42:48 +0400

    Update index.js

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,16 +1,11 @@
-// Link the route / to the AppController
-// Link the route /students and /students/:major
-// to the StudentsController
-
+import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-const express = require('express');
-
 const router = express.Router();
 
-router.get('/students/:major', StudentsController.getAllStudentsByMajor);
-router.get('/students', StudentsController.getAllStudents);
 router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-module.exports = router;
+export default router;
