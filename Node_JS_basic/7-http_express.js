c5e23b02298e7658c3fd6beec1aa10c3e24fe150
ResulShafili <qiyasogluzefer@gmail.com>
2026-05-30 22:59:53 +0400

    Create 7-http_express.js

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,5 @@
+const express = require('express'); const fs = require('fs'); const app = express();
+function getStudentsReport(path) { return fs.promises.readFile(path, 'utf8').then((data) => { const rows = data.split('\n').filter((line) => line.trim() !== ''); const students = rows.slice(1); const byField = {}; students.forEach((student) => { const [firstname, , , field] = student.split(','); if (!byField[field]) { byField[field] = []; } byField[field].push(firstname); }); const lines = [`Number of students: ${students.length}`]; Object.keys(byField).forEach((field) => { lines.push(`Number of students in ${field}: ${byField[field].length}. List: ${byField[field].join(', ')}`); }); return lines.join('\n'); }).catch(() => { throw new Error('Cannot load the database'); }); }
+app.get('/', (req, res) => { res.send('Hello Holberton School!'); });
+app.get('/students', (req, res) => { const dbPath = process.argv[2]; getStudentsReport(dbPath).then((report) => { res.send(`This is the list of our students\n${report}`); }).catch((error) => { res.send(`This is the list of our students\n${error.message}`); }); });
+app.listen(1245); module.exports = app;
